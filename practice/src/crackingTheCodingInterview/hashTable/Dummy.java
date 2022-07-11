21077b2a6812584ddc46860ac5c4e1b9bcf6d76f
AlexandreBessard <alexandre.bessard@gmail.com>
2022-07-11 19:21:11 -0400

    Dummy & HashTable & Main

---
+++ practice/src/crackingTheCodingInterview/hashTable/Dummy.java
@@ -0,0 +1,20 @@
+package crackingTheCodingInterview.hashTable;
+
+public class Dummy {
+    private String name;
+    private int age;
+    public Dummy(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    @Override
+    public String toString() {
+        return "name:" + name + ", age: " + age;
+    }
+}
---
+++ practice/src/crackingTheCodingInterview/hashTable/Main.java
@@ -0,0 +1,13 @@
+package crackingTheCodingInterview.hashTable;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Dummy alex = new Dummy("Alex", 30);
+        Dummy clarisse = new Dummy("Clarisse", 27);
+        Dummy pascale = new Dummy("Pascale", 50);
+    }
+
+
+
+}
