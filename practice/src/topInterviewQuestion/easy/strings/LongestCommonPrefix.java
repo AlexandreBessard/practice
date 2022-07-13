53b431fdf73096d068bbb16708768276347ad3be
AlexandreBessard <alexandre.bessard@gmail.com>
2022-07-13 18:55:12 -0400

    LongestCommonPrefix

--- practice/src/topInterviewQuestion/easy/strings/LongestCommonPrefix.java
+++ practice/src/topInterviewQuestion/easy/strings/LongestCommonPrefix.java
@@ -8,9 +8,42 @@ public class LongestCommonPrefix {
      */
     public static void main(String[] args) {
         String[] strs = {"flower","flow","flight"};
+        String[] strs1 = {"leetcode","leet","lee", "le"};
         System.out.println(longestCommonPrefix(strs));
+        System.out.println(longestCommonPrefixDividAndConquer(strs1));
     }
-
+    /*
+    Approach 2: Divide & Conquer
+    Time complexity: O(S) where S in the number of all characters.
+    Space complexity: O(m . log n)
+    there are 'log n' recursive calls, each store 'm' space to store the result.
+     */
+    static String longestCommonPrefixDividAndConquer(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+    private static String longestCommonPrefix(String[] strs,
+                                              int l,
+                                              int r)
+    {
+        if(l == r)
+            return strs[l];
+        int mid = (l + r) / 2;
+        String lcpLeft = longestCommonPrefix(strs, l, mid);
+        String lcpRight = longestCommonPrefix(strs, mid + 1, r);
+        return commonPrefix(lcpLeft, lcpRight);
+    }
+    private static String commonPrefix(String left, String right) {
+        int min = Math.min(left.length(), right.length());
+        for(int i = 0; i < min; i++) {
+            if(left.charAt(i) != right.charAt(i)) {
+                return left.substring(0, i);
+            }
+        }
+        return left.substring(0, min);
+    }
 
     //Approach 1: Horizontal scanning
     /*
