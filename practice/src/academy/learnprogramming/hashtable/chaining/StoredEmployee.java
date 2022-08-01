7201e2c483f6f603dd0b298866fd9d2ab713f0b2
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-01 18:46:45 -0400

    hashtables

---
+++ practice/src/academy/learnprogramming/hashtable/chaining/StoredEmployee.java
@@ -0,0 +1,13 @@
+package academy.learnprogramming.hashtable.chaining;
+
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+
+}
---
+++ practice/src/academy/learnprogramming/hashtable/linearProbing/StoredEmployee.java
@@ -0,0 +1,12 @@
+package academy.learnprogramming.hashtable.linearProbing;
+
+public class StoredEmployee {
+
+    public String key;
+    public Employee employee;
+
+    public StoredEmployee(String key, Employee employee) {
+        this.key = key;
+        this.employee = employee;
+    }
+}
