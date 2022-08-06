8b708904f9cdf238cc4f438c919f93d5e4a156cc
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-06 18:09:01 -0400

    Refactoring fixes

--- practice/src/topInterviewQuestion/medium/sortingAndSearching/SearchInRotatedSortedArray.java
+++ practice/src/topInterviewQuestion/medium/sortingAndSearching/SearchInRotatedSortedArray.java
@@ -10,6 +10,47 @@ public class SearchInRotatedSortedArray {
         System.out.println(search(nums2, 0));
     }
 
+
+    //Approach 2: BinarySearch One pass
+    /*
+    Time complexity: O(log N)
+    Space complexity: O(1)
+     */
+    public int searchOnePass(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while(start <= end)
+        {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            else if (nums[mid] >= nums[start])
+            {
+                if(target >= nums[start] && target < nums[mid])
+                {
+                    end = mid - 1;
+                }
+                else
+                {
+                    start = mid + 1;
+                }
+            }
+            else
+            {
+                if(target <= nums[end] && target > nums[mid]) {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+
     private static int[] nums;
     private static int target;
     private static int find_rotate_index(int left, int right) {
@@ -59,6 +100,8 @@ public class SearchInRotatedSortedArray {
         int n = nums.length;
         if (n == 1)
             return nums[0] == target ? 0 : -1;
+        // rotation_index splits array in two parts.
+        // Compare nums[0] and target to identify in which part one has to look for target.
         int rotate_index = find_rotate_index(0, n - 1);
         // if target is the smallest element
         if (nums[rotate_index] == target)
