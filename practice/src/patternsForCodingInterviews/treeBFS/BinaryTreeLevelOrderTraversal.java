e93bbaec6b05cad8f2dc5f6e605552ec6cfc5ea7
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-13 14:03:00 -0400

    BinaryTreeLevelOrderTraversal

---
+++ practice/src/patternsForCodingInterviews/treeBFS/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,53 @@
+package patternsForCodingInterviews.treeBFS;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+//https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743844952_44Unit
+public class BinaryTreeLevelOrderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<List<Integer>> result = traverse(root);
+        System.out.println(result);
+    }
+    /*
+    Time: O(N), we traverse each node once.
+    Space: O(N) -> caused by the Queue
+     */
+    static List<List<Integer>> traverse(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while( ! queue.isEmpty()) {
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for(int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                //Add the node to the current level
+                currentLevel.add(currentNode.val);
+                //Insert children
+                if(currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+            result.add(currentLevel);
+        }
+        return result;
+    }
+
+
+
+}
---
+++ practice/src/patternsForCodingInterviews/treeBFS/TreeNode.java
@@ -0,0 +1,10 @@
+package patternsForCodingInterviews.treeBFS;
+
+public class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
