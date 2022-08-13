c7f85b804adceee00fc84b4c0e05dd2113bb1961
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-13 11:27:14 -0400

    FirstUniqueCharacterInAString

--- practice/src/topInterviewQuestion/easy/strings/FirstUniqueCharacterInAString.java
+++ practice/src/topInterviewQuestion/easy/strings/FirstUniqueCharacterInAString.java
@@ -1,15 +1,66 @@
 package topInterviewQuestion.easy.strings;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class FirstUniqueCharacterInAString {
 
     public static void main(String[] args) {
-        String s = "loveleetcode";
+        String s = "lovelee";
         //First Unique character is index 2 : 'v'
         System.out.println(firstUniqueChar(s));
+        System.out.println(firstUniqueCharacterLinkedHashMap(s));
+        System.out.println(firstUniqueCharacterWithArray(s));
+
     }
+
+
+    //Same logic as below with one pass by using an array instead of LinkedHashMap
+    static int firstUniqueCharacterWithArray(String s) {
+        Set<Character> set = new HashSet<>();
+        int[] pos = new int[26];
+        Arrays.fill(pos, -1);
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if( ! set.contains(c)){
+                set.add(c);
+                pos[c - 'a'] = i;
+            } else {
+                pos[c - 'a'] = -1;
+            }
+        }
+        int minIndex = Integer.MAX_VALUE;
+        for(int i = 0; i < 26; i++) {
+            if(pos[i] != -1) {
+                minIndex = Math.min(minIndex, pos[i]);
+            }
+        }
+        return minIndex == Integer.MAX_VALUE ? -1 : minIndex;
+    }
+
+
+    /*
+    One pass using LinkedHashMap
+    Time: O(n)
+    Space: O(n) caused by map and set
+     */
+    static int firstUniqueCharacterLinkedHashMap(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        Set<Character> set = new HashSet<>();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(set.contains(c)) {
+                if(map.containsKey(c)) {
+                    map.remove(c);
+                }
+            } else {
+                map.put(c, i);
+                set.add(c);
+            }
+        }
+        return map.size() == 0 ? - 1 : map.entrySet().iterator().next().getValue();
+    }
+
+
 
     /*
     Time complexity: O(N), since we go through the string of length N two times.
