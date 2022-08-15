9ed3885c9960a2308d283289a108965b46122008
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-15 11:18:35 -0400

    MergeSort & QuickSort

--- practice/src/algorithms/dividAndConquer/MergeSort.java
+++ practice/src/algorithms/dividAndConquer/MergeSort.java
@@ -1,7 +1,7 @@
 package algorithms.dividAndConquer;
 
 import java.util.Arrays;
-
+//https://leetcode.com/explore/learn/card/recursion-ii/470/divide-and-conquer/2868/
 public class MergeSort {
 
     public static void main(String[] args) {
@@ -16,18 +16,22 @@ public class MergeSort {
     static int[] merge_sort(int[] input) {
         if(input.length <= 1)
             return input;
-        int pivot = input.length / 2;
+        int pivot = input.length / 2; //Divide
+        //we recursively sort the sublists in the previous step (Conquer)
         int[] left_list = merge_sort(Arrays.copyOfRange(input, 0, pivot));
         int[] right_list = merge_sort(Arrays.copyOfRange(input, pivot, input.length));
+        //Merge the sorted sublists in the above step (Combine)
         return merge(left_list, right_list);
     }
     private static int[] merge(int[] left_list, int[] right_list) {
+        //Create a new array to place our elements in order to be sorted
         int[] ret = new int[left_list.length + right_list.length];
         int left_cursor = 0;
         int right_cursor = 0;
         int ret_cursor = 0;
         while(left_cursor < left_list.length
                 && right_cursor < right_list.length) {
+            //Smaller placed to the left side of this new array 'ret'
             if(left_list[left_cursor] < right_list[right_cursor]) {
                 ret[ret_cursor++] = left_list[left_cursor++];
             } else {
---
+++ practice/src/algorithms/dividAndConquer/QuickSort.java
@@ -0,0 +1,46 @@
+package algorithms.dividAndConquer;
+
+public class QuickSort {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 5, 3, 2, 8, 7, 6, 4};
+        quickSort(nums);
+        for(int i : nums) {
+            System.out.print(i + ", ");
+        }
+    }
+
+    /* Sorts an array in the ascending order in O(n log n) time */
+    /*
+    Time: O(NlogN)
+     */
+    public static void quickSort(int[] lst) {
+        int n = lst.length;
+        qsort(lst, 0, n - 1);
+    }
+    private static void qsort(int[] lst, int lo, int hi) {
+        if(lo < hi) {
+            int p = partition(lst, lo, hi);
+            qsort(lst, lo, p - 1);
+            qsort(lst, p + 1, hi);
+        }
+    }
+    private static int partition(int[] lst, int lo, int hi) {
+        /* Picks the last element hi as a pivot and returns the index of pivot value in the sorted array */
+        int pivot = lst[hi];
+        int i = lo;
+        for(int j = lo; j < hi; j++) {
+            if(lst[j] < pivot) {
+                //swap
+                int tmp = lst[i];
+                lst[i] = lst[j];
+                lst[j] = tmp;
+                i++;
+            }
+        }
+        int tmp = lst[i];
+        lst[i] = lst[hi];
+        lst[hi] = tmp;
+        return i;
+    }
+}
