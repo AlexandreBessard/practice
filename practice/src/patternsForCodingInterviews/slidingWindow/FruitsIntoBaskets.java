1e4cfcf8816f8bc2b05164d47bf00273dc600bf1
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-16 15:40:53 -0400

    FruitsIntoBaskets & LongestSubstringWithKDistinctCharacters

---
+++ practice/src/patternsForCodingInterviews/slidingWindow/FruitsIntoBaskets.java
@@ -0,0 +1,44 @@
+package patternsForCodingInterviews.slidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628541018393_2Unit
+public class FruitsIntoBaskets {
+
+    public static void main(String[] args) {
+        System.out.println("Maximum number of fruits: " +
+                findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        System.out.println("Maximum number of fruits: " +
+                findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
+    }
+
+    /*
+    Time: O(N) where N is the number of characters in the input array.
+    Space: O(1), there can be a maximum of three types of fruits stored in the
+    frequency map.
+     */
+    public static int findLength(char[] arr) {
+        int windowStart = 0;
+        int maxLength = 0;
+        Map<Character, Integer> fruitFrequencyMap = new HashMap<>();
+        //Try to extend the range [windowStart, windowsEnd]
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+            fruitFrequencyMap.put(arr[windowEnd],
+                    fruitFrequencyMap.getOrDefault(
+                            arr[windowEnd], 0) + 1);
+            //shrink the sliding window until we are left with '2' fruits in
+            //the frequency
+            while(fruitFrequencyMap.size() > 2) {
+                fruitFrequencyMap.put(arr[windowStart],
+                        fruitFrequencyMap.get(arr[windowStart]) - 1);
+                if(fruitFrequencyMap.get(arr[windowStart]) == 0) {
+                    fruitFrequencyMap.remove(arr[windowStart]);
+                }
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, (windowEnd - windowStart) + 1);
+        }
+        return maxLength;
+    }
+}
---
+++ practice/src/patternsForCodingInterviews/slidingWindow/LongestSubstringWithKDistinctCharacters.java
@@ -0,0 +1,49 @@
+package patternsForCodingInterviews.slidingWindow;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628541009794_1Unit
+public class LongestSubstringWithKDistinctCharacters {
+
+    public static void main(String[] args) {
+        System.out.println("Length of the longest substring: "
+                + findLength("araaci", 2));
+        System.out.println("Length of the longest substring: "
+                + findLength("araaci", 1));
+        System.out.println("Length of the longest substring: "
+                + findLength("cbbebi", 3));
+    }
+
+    /*
+    Given a string, find the length of the
+    longest substring in it with no more than K distinct characters.
+     */
+    public static int findLength(String str, int k) {
+        if(str == null || str.length() == 0 || str.length() < k)
+            throw new IllegalArgumentException();
+        int windowStart = 0;
+        int maxLength = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        // in the following loop we'll try to extend the range
+        // [windowStart, windowEnd]
+        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+            char rightChar = str.charAt(windowEnd);
+            charFrequencyMap.put(rightChar,
+                    charFrequencyMap.getOrDefault(rightChar, 0) + 1);
+            // shrink the sliding window, until we are left with 'k'
+            // distinct characters in the frequency map
+            while(charFrequencyMap.size() > k) {
+                char leftChar = str.charAt(windowStart);
+                charFrequencyMap.put(leftChar,
+                        charFrequencyMap.get(leftChar) - 1);
+                if(charFrequencyMap.get(leftChar) == 0) {
+                    charFrequencyMap.remove(leftChar);
+                }
+                windowStart++; //Shrink the window
+            }
+            maxLength = Math.max(maxLength, (windowEnd - windowStart) + 1);
+        }
+        return maxLength;
+    }
+}
