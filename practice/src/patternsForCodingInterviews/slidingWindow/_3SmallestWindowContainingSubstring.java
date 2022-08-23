f9012dbf68eb7f206facf793cd6118d843f3b190
AlexandreBessard <alexandre.bessard@gmail.com>
2022-08-22 21:43:46 -0400

    _3SmallestWindowContainingSubstring

--- practice/src/patternsForCodingInterviews/slidingWindow/_3SmallestWindowContainingSubstring.java
+++ practice/src/patternsForCodingInterviews/slidingWindow/_3SmallestWindowContainingSubstring.java
@@ -13,35 +13,37 @@ public class _3SmallestWindowContainingSubstring {
         System.out.println(findSubstring("adcad", "abc"));
     }
 
+    /*
+    Time: O(N + M)
+    Space: O(M)
+     */
     public static String findSubstring(String str, String pattern) {
-        int windowStart = 0;
-        int matched = 0;
-        int minLength = str.length() + 1;
-        int subStrStart = 0;
-        Map<Character, Integer> charFrequency = new HashMap<>();
-        for(char chr : pattern.toCharArray()) {
-            charFrequency.put(chr, charFrequency.getOrDefault(chr, 0) + 1);
-        }
-        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+        int windowStart = 0, matched = 0, minLength = str.length() + 1, subStrStart = 0;
+        Map<Character, Integer> charFrequencyMap = new HashMap<>();
+        for (char chr : pattern.toCharArray())
+            charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+        // try to extend the range [windowStart, windowEnd]
+        for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
             char rightChar = str.charAt(windowEnd);
-            if(charFrequency.containsKey(rightChar)) {
-                charFrequency.put(rightChar, charFrequency.get(rightChar) - 1);
-                if(charFrequency.get(rightChar) == 0) { //Count every matching of a character
+            if (charFrequencyMap.containsKey(rightChar)) {
+                charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+                if (charFrequencyMap.get(rightChar) >= 0) // count every matching of a character
                     matched++;
-                }
             }
-            //Shrink the window if we can, finish as soon as we remove a matched character
-            while(matched == pattern.length()) {
-                if(minLength > windowEnd - windowStart + 1) { //get min length of the smallest substring
+            // shrink the window if we can, finish as soon as we remove a matched character
+            while (matched == pattern.length()) {
+                if (minLength > windowEnd - windowStart + 1) {
                     minLength = windowEnd - windowStart + 1;
                     subStrStart = windowStart;
                 }
                 char leftChar = str.charAt(windowStart++);
-                if(charFrequency.containsKey(leftChar)) {
-                    if(charFrequency.get(leftChar) == 0){
+                if (charFrequencyMap.containsKey(leftChar)) {
+                    // note that we could have redundant matching characters, therefore we'll
+                    // decrement the matched count only when a useful occurrence of a matched
+                    // character is going out of the window
+                    if (charFrequencyMap.get(leftChar) == 0)
                         matched--;
-                    }
-                    charFrequency.put(leftChar, charFrequency.get(leftChar) + 1);
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
                 }
             }
         }
