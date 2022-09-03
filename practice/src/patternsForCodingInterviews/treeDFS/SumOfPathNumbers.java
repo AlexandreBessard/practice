49c8feef05170c6a0ed65faf958d939942d66299
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-03 18:49:36 -0400

    SumOfPathNumbers

--- practice/src/patternsForCodingInterviews/treeDFS/SumOfPathNumbers.java
+++ practice/src/patternsForCodingInterviews/treeDFS/SumOfPathNumbers.java
@@ -1,4 +1,5 @@
 package patternsForCodingInterviews.treeDFS;
+
 //https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628743939828_56Unit
 public class SumOfPathNumbers {
 
@@ -11,24 +12,53 @@ public class SumOfPathNumbers {
         root.right.right = new TreeNode(5);
         System.out.println("Total Sum of Path Numbers: " +
                 SumOfPathNumbers.findSumOfPathNumbers(root));
+
+        System.out.println(findRootToLeafPathNumbersStr(root));
     }
 
     public static int findSumOfPathNumbers(TreeNode root) {
         return findRootToLeafPathNumbers(root, 0);
     }
+
     /*
     Time complexity: O(N) where N is the total number of nodes in the tree.
     Space complexity: O(N) Space for recursion stack.
      */
     private static int findRootToLeafPathNumbers(TreeNode current, int pathSum) {
-        if(current == null)
+        if (current == null)
             return 0;
+        //See below for alternative solution by using StringBuilder
         pathSum = 10 * pathSum + current.val;
-        if(current.left == null && current.right == null)
+        if (current.left == null && current.right == null)
             return pathSum;
         //Traverse left and right
         return findRootToLeafPathNumbers(current.left, pathSum)
                 + findRootToLeafPathNumbers(current.right, pathSum);
+    }
+
+    //Using StringBuilder
+    private static int findRootToLeafPathNumbersStr(TreeNode root) {
+        int result = 0;
+        return helper(root, new StringBuilder(), result);
+    }
+
+    private static int helper(TreeNode node,
+                              StringBuilder path,
+                              int result) {
+        if (node == null)
+            return 0;
+        //Add to the path
+        path.append(node.val);
+        //It is a leaf node
+        if (node.left == null && node.right == null) {
+            return Integer.parseInt(path.toString());
+        }
+        result = helper(node.left, path, result);
+        //Remove the latest String number format
+        path.deleteCharAt(path.length() - 1);
+        result += helper(node.right, path, result);
+        //Return total of that path including left and right paths
+        return result;
     }
 
 }
