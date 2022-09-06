827c79e3e481884d936d2300613d336113d3cd87
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-06 12:20:21 -0400

    Subsets_2 & ReadNCharactersGivenRead4

---
+++ practice/src/algorithms/backtracking/Subsets_2.java
@@ -0,0 +1,46 @@
+package algorithms.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/subsets-ii/
+public class Subsets_2 {
+
+    public static void main(String[] args) {
+        var obj = new Subsets_2();
+
+        List<List<Integer>> resultBacktracking = obj.subsetsWithDup(new int[]{1, 2, 3});
+        for (List<Integer> l : resultBacktracking) {
+            System.out.println(l);
+        }
+    }
+
+    //Subsets II (contains duplicates) : https://leetcode.com/problems/subsets-ii/
+    /*
+    Time: O(N * 2n) to generate all subsets and then copy them into output list
+    Space: O(N), to maintain the list and modifying list in place. We do not count space for output
+     */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> output = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(output, new ArrayList<>(), nums, 0);
+        return output;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> currList, int[] nums, int start) {
+        list.add(new ArrayList<>(currList));
+        //Iterate all possible candidate
+        for (int i = start; i < nums.length; i++) {
+            if (i > start && nums[i] == nums[i - 1]) continue; // skip duplicates
+            //Candidate solution
+            currList.add(nums[i]);
+            //Move on to the next elements
+            backtrack(list, currList, nums, i + 1);
+            //backtrack removing the number before moving onto the next
+            currList.remove(currList.size() - 1);
+        }
+
+    }
+
+}
---
+++ practice/src/topInterviewQuestion/facebook/arraysAndStrings/ReadNCharactersGivenRead4.java
@@ -0,0 +1,66 @@
+package topInterviewQuestion.facebook.arraysAndStrings;
+
+//https://leetcode.com/explore/interview/card/facebook/5/array-and-strings/268/
+public class ReadNCharactersGivenRead4 extends Reader4 {
+
+    /*
+    File file("abcde"); // File is "abcde", initially file pointer (fp) points to 'a'
+    char[] buf4 = new char[4]; // Create buffer with enough space to store characters
+    read4(buf4); // read4 returns 4. Now buf4 = "abcd", fp points to 'e'
+    read4(buf4); // read4 returns 1. Now buf4 = "e", fp points to end of file
+    read4(buf4); // read4 returns 0. Now buf4 = "", fp points to end of file
+     */
+
+    /*
+    Input: file = "abc", n = 4
+    Output: 3
+    Explanation: After calling your read method, buf
+    should contain "abc". We read a total of 3 characters from the file, so return 3.
+    Note that "abc" is the file's content, not buf. buf is the
+    destination buffer that you will have to write the results to.
+
+
+    Input: file = "abcdABCD1234", n = 12
+Output: 12
+Explanation: After calling your read method, buf
+should contain "abcdABCD1234". We read a total of 12 characters from the file, so return 12.
+     */
+    public static void main(String[] args) {
+        String file = "abcde";
+        int n = 5;
+        var obj = new ReadNCharactersGivenRead4();
+        System.out.println(obj.read(file.toCharArray(), n));
+    }
+
+    public int read(char[] buf, int n) {
+        char[] temp = new char[4];  //Store our read chars from Read4
+        int total = 0;
+        while (total < n) {
+            /*Read and store characters in Temp. Count will store total chars read from Read4*/
+            int count = read4(temp);
+            /*Even if we read 4 chars from Read4,
+            we don't want to exceed N and only want to read chars till N.*/
+            count = Math.min(count, n - total);
+            //Transfer all the characters read from Read4 to our buffer
+            for (int i = 0; i < count; i++) {
+                buf[total] = temp[i];
+                total++;
+            }
+            //done. We can't read more characters.
+            if (count < 4) break;
+        }
+        return total;
+    }
+
+}
+
+class Reader4 {
+    /*
+    Parameter:  char[] buf4
+    Returns:    int
+    buf4[] is a destination, not a source. The results from read4 will be copied to buf4[].
+     */
+    int read4(char[] buf4) {
+        return buf4.length;
+    }
+}
