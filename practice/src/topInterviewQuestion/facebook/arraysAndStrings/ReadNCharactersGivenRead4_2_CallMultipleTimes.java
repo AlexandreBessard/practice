5b182197467fe3f50872cfb2d7e3ba7c99b25afb
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-07 16:57:08 -0400

    ReadNCharactersGivenRead4_2_CallMultipleTimes

--- practice/src/topInterviewQuestion/facebook/arraysAndStrings/ReadNCharactersGivenRead4_2_CallMultipleTimes.java
+++ practice/src/topInterviewQuestion/facebook/arraysAndStrings/ReadNCharactersGivenRead4_2_CallMultipleTimes.java
@@ -2,6 +2,29 @@ package topInterviewQuestion.facebook.arraysAndStrings;
 //https://leetcode.com/explore/interview/card/facebook/5/array-and-strings/269/
 public class ReadNCharactersGivenRead4_2_CallMultipleTimes {
 
-    //TODO: need to be done
+    //TODO: need to be reviewed
+    public static void main(String[] args) {
+
+    }
+
+
+    private int buffPtr = 0;
+    private int buffCnt = 0;
+    private char[] buff = new char[4];
+    public int read(char[] buf, int n) {
+        int ptr = 0;
+        while (ptr < n) {
+            if (buffPtr == 0) {
+                //TODO: must implement interface
+                //buffCnt = read4(buff);
+            }
+            if (buffCnt == 0) break;
+            while (ptr < n && buffPtr < buffCnt) {
+                buf[ptr++] = buff[buffPtr++];
+            }
+            if (buffPtr >= buffCnt) buffPtr = 0;
+        }
+        return ptr;
+    }
 
 }
