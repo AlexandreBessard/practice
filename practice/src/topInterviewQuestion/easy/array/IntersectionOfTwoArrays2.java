362e875ce39f086eaae6403dec7e55238e5759a6
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-10 17:12:03 -0400

    IntersectionOfTwoArrays2

--- practice/src/topInterviewQuestion/easy/array/IntersectionOfTwoArrays2.java
+++ practice/src/topInterviewQuestion/easy/array/IntersectionOfTwoArrays2.java
@@ -1,44 +1,48 @@
 package topInterviewQuestion.easy.array;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/674/
 public class IntersectionOfTwoArrays2 {
 
     public static void main(String[] args) {
         int[] nums1 = {4, 9, 5};
-        int[] nums2 = {9,4,9,8,4};
+        int[] nums2 = {9, 4, 9, 8, 4};
         //Output: [4, 9]
-        for(int i : intersectSort(nums1, nums2)) {
+        for (int i : intersectSort(nums1, nums2)) {
             System.out.print(i + ", ");
         }
     }
 
-    //Approach 2: Sort
+    //Approach 2: In case if the arrays are sorted
     /*
-    Time complexity: O(n log n + m log m) -> we sort two arrays independently.
-    Space complexity: O(log n + log m)
+    Time complexity: O(max(N, M))
      */
     static int[] intersectSort(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        int i = 0, j = 0, k = 0;
-        while (i < nums1.length && j < nums2.length) {
-            if (nums1[i] < nums2[j]) {
-                ++i;
-            } else if (nums1[i] > nums2[j]) {
-                ++j;
+        int n = nums1.length, m = nums2.length;
+        int i = 0, j = 0;
+        List<Integer> list = new ArrayList<>();
+        while (i < n && j < m) {
+            if (nums1[i] == nums2[j]) {
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
             } else {
-                nums1[k++] = nums1[i++];
-                ++j;
+                j++;
             }
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+        int[] ret = new int[list.size()];
+        for (int k = 0; k < list.size(); k++) {
+            ret[k] = list.get(k);
+        }
+        return ret;
     }
 
-        //Approach 1: Hash Table
+    //Approach 1: Hash Table IF the array is NOT sorted
     /*
     Time complexity: O(n + m) -> where n and m are the lengths of the arrays.
     Iterate through the first and second array.
@@ -47,22 +51,21 @@ public class IntersectionOfTwoArrays2 {
     the smaller array
      */
     static int[] intersectHashMap(int[] nums1, int[] nums2) {
-        //nums1 is always the smallest array
-        if (nums1.length > nums2.length) {
-            return intersectHashMap(nums2, nums1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i : nums1) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
         }
-        HashMap<Integer, Integer> m = new HashMap<>();
-        for (int n : nums1) {
-            m.put(n, m.getOrDefault(n, 0) + 1);
-        }
-        int k = 0;
-        for (int n : nums2) {
-            int cnt = m.getOrDefault(n, 0);
-            if (cnt > 0) {
-                nums1[k++] = n;
-                m.put(n, cnt - 1);
+        List<Integer> list = new ArrayList<>();
+        for (int i : nums2) {
+            if (map.get(i) != null && map.get(i) > 0) {
+                list.add(i);
+                map.put(i, map.get(i) - 1);
             }
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+        int[] ret = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+        return ret;
     }
 }
