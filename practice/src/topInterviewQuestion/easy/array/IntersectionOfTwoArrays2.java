c93d3ee91c458dadf65fbcbecb7c4a263b54e8f5
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-10 17:33:45 -0400

    IntersectionOfTwoArrays2

--- practice/src/topInterviewQuestion/easy/array/IntersectionOfTwoArrays2.java
+++ practice/src/topInterviewQuestion/easy/array/IntersectionOfTwoArrays2.java
@@ -9,9 +9,66 @@ public class IntersectionOfTwoArrays2 {
         int[] nums1 = {4, 9, 5};
         int[] nums2 = {9, 4, 9, 8, 4};
         //Output: [4, 9]
+        /* Use approach 1 & 2 for this one
         for (int i : intersectSort(nums1, nums2)) {
             System.out.print(i + ", ");
         }
+         */
+        //Use approach 3 for this case:
+        nums1 = new int[]{9, 4, 9, 8, 4};
+        nums2 = new int[]{4, 9, 5};
+        System.out.println(Arrays.toString(intersectBinarySearch(nums1, nums2)));
+
+    }
+
+    //Approach 3: In case if the array1 is longer than array2
+    //https://leetcode.com/problems/intersection-of-two-arrays-ii/discuss/1808056/Java-Binary-Search
+    /*
+    Let's say nums1 is K size. Then we should do binary search for every element in nums1.
+    Each lookup is O(log N), and if we do K times, we have O(K log N).
+If K this is small enough, O(K log N) < O(max(N, M)).
+Otherwise, we have to use the previous two pointers method.
+let's say A = [1, 2, 2, 2, 2, 2, 2, 2, 1], B = [2, 2].
+For each element in B, we start a binary search in A. To deal with duplicate entry,
+once you find an entry, all the duplicate element is around that that index, so you can do linear search scan afterward.
+Time complexity, O(K(logN) + N). Plus N is worst case scenario which you have to linear scan every element in A.
+But on average, that shouldn't be the case. so I'd say the Time complexity is O(K(logN) + c), c (constant)
+is number of linear scan you did.
+//Space: O(N) if we need to sort the array
+     */
+    public static int[] intersectBinarySearch(int[] nums1, int[] nums2) {
+        if(nums2.length < nums1.length) {
+            intersectBinarySearch(nums2, nums1);
+        }
+        //nums1 has equal or smaller length than nums2
+        Arrays.sort(nums1); // 4, 5, 9
+        Arrays.sort(nums2); // 4, 4, 8, 9, 9
+        List<Integer> result = new ArrayList<>();
+        int leftIndex = 0;
+        for(int num : nums1) {
+            int index = binarySearch(nums2, num, leftIndex);
+            if(index != -1) {
+                result.add(num);
+                leftIndex = index + 1;
+            }
+        }
+        return result.stream().mapToInt(Integer::intValue).toArray();
+    }
+    private static int binarySearch(int[] nums, int target, int left) {
+        int right = nums.length - 1;
+        int index = -1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target) {
+                index = mid;
+                right = mid - 1;
+            } else if(nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return index;
     }
 
     //Approach 2: In case if the arrays are sorted
