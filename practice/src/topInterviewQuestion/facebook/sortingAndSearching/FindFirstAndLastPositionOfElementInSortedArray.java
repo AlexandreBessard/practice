3243b05ee435760ac34c202026545764eb4c1792
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-17 18:23:52 -0400

    FindFirstAndLastPositionOfElementInSortedArray

--- practice/src/topInterviewQuestion/facebook/sortingAndSearching/FindFirstAndLastPositionOfElementInSortedArray.java
+++ practice/src/topInterviewQuestion/facebook/sortingAndSearching/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 //https://leetcode.com/explore/interview/card/facebook/54/sorting-and-searching-3/3030/
 public class FindFirstAndLastPositionOfElementInSortedArray {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int[] nums = {5,7,7,8,8,10};
         int target = 8;
         var obj = new FindFirstAndLastPositionOfElementInSortedArray();
@@ -18,11 +18,15 @@ public class FindFirstAndLastPositionOfElementInSortedArray {
     Time: O(log N)
     Space: O(1)
      */
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange(int[] nums, int target) throws InterruptedException {
         int[] result = new int[2];
-        result[0] = findFirst(nums, target);
-        result[1] = findLast(nums, target);
-        return result;
+        Thread t1 = new Thread(() -> result[0] = findFirst(nums, target));
+        Thread t2 = new Thread(() -> result[1] = findLast(nums, target));
+        t1.start();
+        t2.start();
+        t1.join(); // wait until t1 Thread is finished
+        t2.join(); //wait until t2 Thread is finised
+        return result; // To return the result must wait until both t1 and t2 are finished to have our final result
     }
     //5,7,8,8,7,10
     private int findLast(int[] nums, int target) {
