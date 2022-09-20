04b6f9d29fdd077c0080d8a45fc4a6c1125b8eee
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-20 12:25:51 -0400

    DotProductOfTwoSparseVectors

--- practice/src/topInterviewQuestion/facebook/topMostFrequentQuestions/DotProductOfTwoSparseVectors.java
+++ practice/src/topInterviewQuestion/facebook/topMostFrequentQuestions/DotProductOfTwoSparseVectors.java
@@ -12,6 +12,11 @@ public class DotProductOfTwoSparseVectors {
         var vec1 = new SparseVector(nums1);
         var vec2 = new SparseVector(nums2);
         System.out.println(vec1.dotProduct(vec2));
+        //FOLLOWUP if arrays do not have the same length
+        var vec3 = new SparseVector(new int[]{1, 0, 0, 2, 3});
+        var vec4 = new SparseVector(new int[]{10, 5});
+        //Output: 10
+        System.out.println(vec3.doProductFollowUp(vec4));
     }
 
     //Correction: https://leetcode.com/problems/dot-product-of-two-sparse-vectors/discuss/1522271/Java-O(n)-solution-using-Two-Pointers-with-detailed-explanation-and-Follow-up
@@ -27,6 +32,7 @@ public class DotProductOfTwoSparseVectors {
                     list.add(new int[]{i, nums[i]});
                 }
             }
+            System.out.println();
         }
 
         //Two Pointers
@@ -52,6 +58,47 @@ public class DotProductOfTwoSparseVectors {
                 }
             }
             return dotProduct;
+        }
+
+        //FOLLOW UP
+        //If you get a follow up that for non-sparse array how to optimize solution,
+        // use binary search as the indexes of non-zero elements will be sorted
+        public int doProductFollowUp(SparseVector vec) {
+            //calculate sum such that iteration happens over vector with lesser number of non-zero elements.
+            if (this.list.size() < vec.list.size()){
+                return helper(this, vec);
+            } else {
+                return helper(vec, this);
+            }
+        }
+        private int helper(SparseVector smallVec, SparseVector largeVec) {
+            int result = 0;
+            for(int[] curr : smallVec.list) {
+                // perform binary search to find the curr non-zero element index in larger non-zero element vector.
+                int[] exists = binarySearch(largeVec.list, curr[0]);
+                if(exists[0] == curr[0])
+                    result += exists[1] * curr[1];
+            }
+            return result;
+        }
+        // perform binary search.
+        private int[] binarySearch(List<int[]> listParam, int index) {
+            int[] result = new int[]{-1, 0};
+            int low = 0;
+            int high = listParam.size() - 1;
+            if(index < listParam.get(low)[0] || index > listParam.get(high)[0]) //True, index is OutOfBound
+                return result;
+            while(low <= high) {
+                int mid = low + (high - low) / 2;
+                if(listParam.get(mid)[0] == index) {
+                    return listParam.get(mid);
+                } else if(listParam.get(mid)[0] > index) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return result;
         }
     }
 
