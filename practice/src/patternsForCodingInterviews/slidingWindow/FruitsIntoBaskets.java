b46d74d981df3f24dbffaeb728e4df68613507a9
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-23 16:38:21 -0400

    FruitsIntoBaskets

--- practice/src/patternsForCodingInterviews/slidingWindow/FruitsIntoBaskets.java
+++ practice/src/patternsForCodingInterviews/slidingWindow/FruitsIntoBaskets.java
@@ -9,6 +9,7 @@ public class FruitsIntoBaskets {
     public static void main(String[] args) {
         System.out.println("Maximum number of fruits: " +
                 findLength(new char[] { 'A', 'B', 'C', 'A', 'C' }));
+        //Explanation: 2 baskets, we must have fruits as much as possible. Each basckets can contain 1 type of fruit
         System.out.println("Maximum number of fruits: " +
                 findLength(new char[] { 'A', 'B', 'C', 'B', 'B', 'C' }));
     }
@@ -24,14 +25,10 @@ public class FruitsIntoBaskets {
         Map<Character, Integer> fruitFrequencyMap = new HashMap<>();
         //Try to extend the range [windowStart, windowsEnd]
         for(int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-            fruitFrequencyMap.put(arr[windowEnd],
-                    fruitFrequencyMap.getOrDefault(
-                            arr[windowEnd], 0) + 1);
-            //shrink the sliding window until we are left with '2' fruits in
-            //the frequency
+            fruitFrequencyMap.put(arr[windowEnd], fruitFrequencyMap.getOrDefault(arr[windowEnd], 0) + 1);
+            //shrink the sliding window until we are left with '2' fruits in the frequency
             while(fruitFrequencyMap.size() > 2) {
-                fruitFrequencyMap.put(arr[windowStart],
-                        fruitFrequencyMap.get(arr[windowStart]) - 1);
+                fruitFrequencyMap.put(arr[windowStart], fruitFrequencyMap.get(arr[windowStart]) - 1);
                 if(fruitFrequencyMap.get(arr[windowStart]) == 0) {
                     fruitFrequencyMap.remove(arr[windowStart]);
                 }
