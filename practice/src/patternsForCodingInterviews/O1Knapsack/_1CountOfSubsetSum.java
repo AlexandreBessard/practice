928853108717ace259f33146f51138f1a059ff0a
AlexandreBessard <alexandre.bessard@gmail.com>
2022-09-28 18:56:42 -0400

    Format

--- practice/src/patternsForCodingInterviews/O1Knapsack/_1CountOfSubsetSum.java
+++ practice/src/patternsForCodingInterviews/O1Knapsack/_1CountOfSubsetSum.java
@@ -1,4 +1,5 @@
 package patternsForCodingInterviews.O1Knapsack;
+
 //https://designgurus.org/path-player?courseid=grokking-the-coding-interview&unit=grokking-the-coding-interview_1628744496415_118Unit
 public class _1CountOfSubsetSum {
 
@@ -12,25 +13,27 @@ public class _1CountOfSubsetSum {
 
     static class SubsetSum {
         Integer[][] dp;
+
         public int countSubsets(int[] nums, int S) {
             dp = new Integer[nums.length][S + 1];
             return countSubsetsRecursive(nums, S, 0);
         }
+
         private int countSubsetsRecursive(int[] nums, int S, int currentIndex) {
             //Base case
-            if(S == 0) {
+            if (S == 0) {
                 return 1; //This subset is equal to S
             }
-            if(S <= 0 || currentIndex >= nums.length) {
+            if (S <= 0 || currentIndex >= nums.length) {
                 return 0;
             }
             //Check from cache
-            if(dp[currentIndex][S] != null) {
+            if (dp[currentIndex][S] != null) {
                 return dp[currentIndex][S];
             }
             //Recursive
             int sum1 = 0;
-            if(nums[currentIndex] <= S) {
+            if (nums[currentIndex] <= S) {
                 int newSum = S - nums[currentIndex];
                 sum1 = countSubsetsRecursive(nums, newSum, currentIndex + 1);
             }
