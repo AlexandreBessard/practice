2636d20382479373197d1ae9691639c3202485a9
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-05 11:52:12 -0400

    DiameterOfBinaryTree

--- practice/src/topInterviewQuestion/amazon/treesAndGraphs/DiameterOfBinaryTree.java
+++ practice/src/topInterviewQuestion/amazon/treesAndGraphs/DiameterOfBinaryTree.java
@@ -3,7 +3,13 @@ package topInterviewQuestion.amazon.treesAndGraphs;
 public class DiameterOfBinaryTree {
 
     public static void main(String[] args) {
-
+        TreeNode one = new TreeNode(1);
+        one.left = new TreeNode(2);
+        one.left.left = new TreeNode(4);
+        one.left.right = new TreeNode(5);
+        one.right = new TreeNode(3);
+        var obj = new DiameterOfBinaryTree();
+        System.out.println(obj.diameterOfBinaryTree(one));
     }
 
 
@@ -27,7 +33,7 @@ public class DiameterOfBinaryTree {
         int rightPath = longestPath(node.right);
         //Update the diameter
         ////We're looking for diameter, that why we include left and right at each step.
-        diameter = Math.max(diameter, leftPath + leftPath);
+        diameter = Math.max(diameter, leftPath + rightPath);
         //Return longest path between right or left
         return Math.max(leftPath, rightPath) + 1;
     }
