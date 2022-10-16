55b59f164bf3411a291d0ebc76d0f8818108af0d
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-15 23:27:04 -0400

    MaximalSquare

--- practice/src/topInterviewQuestion/top100likedQuestions/MaximalSquare.java
+++ practice/src/topInterviewQuestion/top100likedQuestions/MaximalSquare.java
@@ -1,6 +1,9 @@
 package topInterviewQuestion.top100likedQuestions;
 
 import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 //https://leetcode.com/problems/maximal-square/
 public class MaximalSquare {
@@ -17,14 +20,56 @@ public class MaximalSquare {
         char[][] matrix2 = {
                 {'1', '0', '1'},
                 {'1', '1', '0'},
-                {'1', '1', '0'}
+                {'1', '1', '1'},
+                {'1', '1', '1'},
+                {'1', '1', '1'}
         };
         char[][] matrix3 = {
                 {'1', '1', '1'},
                 {'1', '1', '1'}
         };
         //System.out.println(maximalSquare(matrix3));
-        System.out.println(maximalSquareDP(matrix3));
+        System.out.println(maximalSquareDP(matrix2));
+        System.out.println(maximalDPTopDown(matrix2));
+    }
+
+    //Explanation: https://www.youtube.com/watch?v=6X7Ha2PrDmM
+    //Recursive: Top down
+    static int ROWS;
+    static int COLS;
+    static char[][] matrix;
+    static int maxLength = 0;
+    //Key: row, Value: column
+    static int[][] cache;
+    static int maximalDPTopDown(char[][] matrix) {
+        MaximalSquare.matrix = matrix;
+        ROWS = matrix.length;
+        COLS = matrix[0].length;
+        cache = new int[ROWS][COLS];
+        for(int i = 0; i < ROWS; i++) {
+            Arrays.fill(cache[i], -1);
+        }
+        helper(0, 0); //top left element
+        return maxLength * maxLength;
+    }
+    static int helper(int row, int col) {
+        //Base case
+        if(row >= ROWS || col >= COLS) {
+            return 0;
+        }
+        if(cache[row][col] == -1) { //True if NOT in the cache
+            int down = helper(row + 1, col); //Check Down
+            int right = helper(row, col + 1); //Right position
+            int diag = helper(row + 1, col + 1); //Check Diagonally
+            cache[row][col] = 0;
+            if(matrix[row][col] == '1') { //
+                //Takes the minimum off all of these 3 values: down, right and diag
+                cache[row][col] = 1 + Math.min(down, Math.min(right, diag));
+                //Get the max length so far
+                maxLength = Math.max(cache[row][col], maxLength);
+            }
+        }
+        return cache[row][col];
     }
 
     //Approach 2: DP
@@ -64,12 +109,13 @@ public class MaximalSquare {
         int maxSquareLen = 0;
         for (int row = 0; row < rows; row++) {
             for (int col = 0; col < cols; col++) {
+                //Top left corner
                 if (matrix[row][col] == '1') { //Check element from top left of the square
                     int squareLen = 1;
                     boolean flag = true; //If false means we do not have a square (0 element in that square)
                     while ((squareLen + row) < rows && (squareLen + col) < cols && flag) {
                         for (int k = col; k <= squareLen + col; k++) { //It true, ensure it is a square check
-                            if (matrix[row + squareLen][k] == '0') { //Check element below
+                            if (matrix[row + squareLen][k] == '0') { //Check element below from top
                                 flag = false;
                                 break;
                             }
