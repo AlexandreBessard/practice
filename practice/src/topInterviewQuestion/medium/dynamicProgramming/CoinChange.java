ae546449dd6926aa645abefc4efea170ab0d9f2d
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-17 23:57:58 -0400

    CoinChange

--- practice/src/topInterviewQuestion/medium/dynamicProgramming/CoinChange.java
+++ practice/src/topInterviewQuestion/medium/dynamicProgramming/CoinChange.java
@@ -11,7 +11,7 @@ public class CoinChange {
         int amount2 = 6;
         int amount = 11; // 5 + 5 + 1
         System.out.println(coinChangeTopDown(coins2, amount2));
-        System.out.println(coinChangeBottomUm(coins2, amount2));
+        System.out.println(coinChangeBottomUm(coins, 3));
     }
 
     //Approach 3: Dynamic Programming: Bottom-Up
@@ -19,13 +19,13 @@ public class CoinChange {
         int max = amount + 1;
         int[] dp = new int[amount + 1];
         Arrays.fill(dp, max);
-        dp[0] = 0;
+        dp[0] = 0; //With amount 0 dp[0] I need 0 coins.
         //i represent the amount
-        for (int i = 1; i <= amount; i++) {
+        for (int i = 1; i <= amount; i++) { //check for each amount from 1 to amount (bottom up) how minimum coins do I need
             //j represent the coins at the index
-            for (int j = 0; j < coins.length; j++) {
-                if (coins[j] <= i) {
-                    dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            for (int coin : coins) {
+                if ((i - coin) >= 0) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1); // +1 represent the number of coins.
                 }
             }
         }
