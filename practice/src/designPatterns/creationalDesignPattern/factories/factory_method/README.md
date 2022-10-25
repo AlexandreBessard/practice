5764988bc7ef309273ed98227f318b86d0500406
AlexandreBessard <alexandre.bessard@gmail.com>
2022-10-24 20:12:13 -0400

    Factory

--- practice/src/designPatterns/creationalDesignPattern/factories/factory_method/README.md
+++ practice/src/designPatterns/creationalDesignPattern/factories/factory_method/README.md
@@ -1,1 +1,4 @@
 https://www.geeksforgeeks.org/factory-method-design-pattern-in-java/
+
+It is a creational design pattern that talks about the creation of an object. 
+Object creation logic is hidden from the client.
--- practice/src/designPatterns/structuralDesignPattern/DecoratorDesignPattern/Main.java
+++ practice/src/designPatterns/structuralDesignPattern/DecoratorDesignPattern/Main.java
@@ -1,11 +1,11 @@
 package designPatterns.structuralDesignPattern.DecoratorDesignPattern;
-
+//Decorator Design pattern
 public class Main {
 
     public static void main(String[] args) {
         Shape circle = new Circle();
-        Shape redCircle = new RedShapeDecorator(new Circle());
-        Shape redRectangle = new RedShapeDecorator(new Rectangle());
+        Shape redCircle = new RedShapeDecorator(new Circle()); //Add new functionalities to the object Circle
+        Shape redRectangle = new RedShapeDecorator(new Rectangle()); //Add new functionalities to the object Rectangle
 
         //circle.draw();
         redCircle.draw();
