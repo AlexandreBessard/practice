0ea005e1e95994bb307ccd23e62b33c61230a196
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-16 19:14:40 -0500

    MoveZeroes & PlusOne

--- practice/src/topInterviewQuestion/easy/array/ValidSudoku.java
+++ practice/src/topInterviewQuestion/easy/array/ValidSudoku.java
@@ -22,6 +22,52 @@ public class ValidSudoku {
         System.out.println(isValidSudokuArrayFixedLength(board));
     }
 
+    //Approach 1: Hash Set
+    /*
+    Time complexity: O(N²) -> traverse array of 2 dimensions.
+    Space complexity: O(n²) if the board is full, need same space as the board.
+     */
+    static boolean isValidSudoku(char[][] board) {
+        int N = 9;
+        //Arrays with each element is a Set which contained Character
+        Set<Character>[] rows = new HashSet[N];
+        Set<Character>[] cols = new HashSet[N];
+        Set<Character>[] boxes = new HashSet[N];
+        for(int r = 0; r < N; r++) {
+            rows[r] = new HashSet<>();
+            cols[r] = new HashSet<>();
+            boxes[r] = new HashSet<>(); // 0 to 8 boxes (total of 9)
+        }
+        for(int r = 0; r < N; r++) {
+            for(int c = 0; c < N; c++) {
+                char val = board[r][c];
+                if(val == '.')
+                    continue;
+                //Check row
+                if(rows[r].contains(val)) {
+                    return false;
+                }
+                rows[r].add(val);
+                if(cols[r].contains(val)) {
+                    return false;
+                }
+                cols[r].add(val);
+                //Check box
+                /*
+                Alternatively, we can use the numbers 0 through 8 to represent these boxes,
+                 where (r/3) * 3 + (c/3) is used calculate
+                 a number in the range from 0 to 8. I.e. the square
+                 located at (r, c) belongs to the box (r/3) * 3 + (c/3).
+                 */
+                int idx = (r / 3) * 3 + c / 3;
+                if(boxes[idx].contains(val)) {
+                    return false;
+                }
+                boxes[idx].add(val);
+            }
+        }
+        return true;
+    }
 
     //Approach 2 : Array of Fixed Length
     /*
@@ -50,50 +96,6 @@ public class ValidSudoku {
                     return false;
                 }
                 boxes[idx][pos] = 1;
-            }
-        }
-        return true;
-    }
-
-
-    //Approach 1: Hash Set
-    /*
-    Time complexity: O(N²) -> traverse array of 2 dimensions.
-    Space complexity: O(n²) if the board is full, need same space as the board.
-     */
-    static boolean isValidSudoku(char[][] board) {
-        int N = 9;
-        Set<Character>[] rows = new HashSet[N];
-        Set<Character>[] cols = new HashSet[N];
-        Set<Character>[] boxes = new HashSet[N];
-        for(int r = 0; r < N; r++) {
-            rows[r] = new HashSet<>();
-            cols[r] = new HashSet<>();
-            boxes[r] = new HashSet<>(); // 0 to 8 boxes (total of 9)
-        }
-        for(int r = 0; r < N; r++) {
-            for(int c = 0; c < N; c++) {
-                char val = board[r][c];
-                if(val == '.')
-                    continue;
-                //Check row
-                if(rows[r].contains(val))
-                    return false;
-                rows[r].add(val);
-                if(cols[r].contains(val))
-                    return false;
-                cols[r].add(val);
-                //Check box
-                /*
-                Alternatively, we can use the numbers 0 through 8 to represent these boxes,
-                 where (r/3) * 3 + (c/3) is used calculate
-                 a number in the range from 0 to 8. I.e. the square
-                 located at (r, c) belongs to the box (r/3) * 3 + (c/3).
-                 */
-                int idx = (r / 3) * 3 + c / 3;
-                if(boxes[idx].contains(val))
-                    return false;
-                boxes[idx].add(val);
             }
         }
         return true;
