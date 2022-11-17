2b08a1d7bd213273b985b4e8f23b6f7fdde729a0
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-16 19:07:51 -0500

    MoveZeroes & PlusOne

--- practice/src/topInterviewQuestion/easy/array/MoveZeroes.java
+++ practice/src/topInterviewQuestion/easy/array/MoveZeroes.java
@@ -5,7 +5,10 @@ import java.util.Arrays;
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/567/
 //Solution: https://leetcode.com/problems/move-zeroes/discuss/172432/THE-EASIEST-but-UNUSUAL-snowball-JAVA-solution-BEATS-100-(O(n))-%2B-clear-explanation
 public class MoveZeroes {
-
+/*
+Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+ */
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
         moveZeroes(nums);
@@ -22,8 +25,8 @@ public class MoveZeroes {
             if(nums[i] == 0) {
                 snowBallSize++;
             } else if(snowBallSize > 0) {
-                int t = nums[i];
-                nums[i] = 0;
+                int t = nums[i]; //Get the current element
+                nums[i] = 0; //Replaced by 0
                 nums[i - snowBallSize] = t;
             }
         }
--- practice/src/topInterviewQuestion/easy/array/PlusOne.java
+++ practice/src/topInterviewQuestion/easy/array/PlusOne.java
@@ -1,7 +1,13 @@
 package topInterviewQuestion.easy.array;
 //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/559/
 public class PlusOne {
+    /*
+    You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+    The digits are ordered from most significant to least significant in left-to-right order.
+    The large integer does not contain any leading 0's.
 
+    Increment the large integer by one and return the resulting array of digits.
+     */
     public static void main(String[] args) {
         int[] digits  = {4,3,2,1};
         //Output: [4, 3, 2, 2]
@@ -13,8 +19,6 @@ public class PlusOne {
         }
     }
 
-
-
     //Approach 1: Schoolbook Addition with Carry
     /*
     Time complexity: O(N)
@@ -22,9 +26,10 @@ public class PlusOne {
      */
     static int[] plusOne(int[] digits) {
         int n = digits.length;
-        for(int idx = n - 1; idx >= 0; idx--) {
-            if(digits[idx] == 9)
+        for(int idx = n - 1; idx >= 0; idx--) { //start at the index of the array
+            if(digits[idx] == 9) {
                 digits[idx] = 0;
+            }
             else {
                 digits[idx]++;
                 return digits;
@@ -32,7 +37,7 @@ public class PlusOne {
         }
         //We are here because all the digits are nines
         digits = new int[n + 1];
-        digits[0] = 1;
+        digits[0] = 1; //Fist element must be 1
         return digits;
     }
 
