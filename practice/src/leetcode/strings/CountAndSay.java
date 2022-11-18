f44b7ec4ed5fa1af24bb560df74f3dc18b41778b
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-18 14:31:34 -0500

    CountAndSay

--- practice/src/leetcode/strings/CountAndSay.java
+++ practice/src/leetcode/strings/CountAndSay.java
@@ -1,7 +1,15 @@
 package leetcode.strings;
 //https://leetcode.com/explore/interview/card/top-interview-questions-medium/103/array-and-strings/4153/
 public class CountAndSay {
+/*
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
 
+countAndSay(1) = "1"
+countAndSay(n) is the way you would "say" the digit string from countAndSay(n-1), which is then converted into a different digit string.
+To determine how you "say" a digit string, split it into the minimal number of substrings such that each substring contains exactly one unique digit. Then for each substring, say the number of digits, then say the digit. Finally, concatenate every said digit.
+
+For example, the saying and conversion for digit string "3322251":
+ */
     /*
     Must understand the logic:
  1.     1
