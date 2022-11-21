0bb4dfd835ad2544ecb7a13e244c163e838aec7e
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-21 15:47:44 -0500

    RemoveDuplicatesFromSortedArray

--- practice/src/leetcode/array/RemoveDuplicatesFromSortedArray.java
+++ practice/src/leetcode/array/RemoveDuplicatesFromSortedArray.java
@@ -1,7 +1,12 @@
 package leetcode.array;
-
+//https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 public class RemoveDuplicatesFromSortedArray {
-
+/*
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+ */
     public static void main(String[] args) {
         int[] nums = {1, 1, 2};
         //Output: 2 (elements) -> {1, 2, _}
@@ -9,16 +14,21 @@ public class RemoveDuplicatesFromSortedArray {
         System.out.println(removeDuplicates(nums2));
     }
 
+    /*
+    Time: O(n)
+    Space: O(1)
+     */
     static int removeDuplicates(int[] nums) {
-        if (nums.length == 0)
+        if (nums.length == 0) { //true if empty
             return 0;
-        int i = 0;
-        for (int j = 1; j < nums.length; j++) {
+        }
+        int i = 0; //Pointer to know where is the first non repeating number
+        for (int j = 1; j < nums.length; j++) { //Loop through each letter
             if (nums[j] != nums[i]) {
-                i++;
+                i++; //We know from i that this is the first non repeating number
                 nums[i] = nums[j];
             }
         }
-        return i + 1;
+        return i + 1; //Latest index where non-repeating + 1
     }
 }
