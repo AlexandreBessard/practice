0c9f1852ba1a759f57a3a6a4bf97c797d4fca3dc
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-22 17:44:51 -0500

    ReverseWordsInAStringIII

--- practice/src/leetcode/strings/ReverseWordsInAStringIII.java
+++ practice/src/leetcode/strings/ReverseWordsInAStringIII.java
@@ -1,16 +1,17 @@
 package leetcode.strings;
+
 //https://leetcode.com/problems/reverse-words-in-a-string-iii/
 public class ReverseWordsInAStringIII {
-/*
-Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
-Example 1:
-Input: s = "Let's take LeetCode contest"
-Output: "s'teL ekat edoCteeL tsetnoc"
+    /*
+    Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+    Example 1:
+    Input: s = "Let's take LeetCode contest"
+    Output: "s'teL ekat edoCteeL tsetnoc"
 
-Example 2:
-Input: s = "God Ding"
-Output: "doG gniD"
- */
+    Example 2:
+    Input: s = "God Ding"
+    Output: "doG gniD"
+     */
     public static void main(String[] args) {
         String s = "Let's take LeetCode contest";
         String s1 = "God Ding";
@@ -25,12 +26,12 @@ Output: "doG gniD"
      */
     public static String reverseWords(String s) {
         char[] chrArray = s.toCharArray();
-        for(int start = 0; start < chrArray.length; start++) {
+        for (int start = 0; start < chrArray.length; start++) {
             //Move to the next element if it is a space
-            if(chrArray[start] != ' ') { //True if non-space (letter)
+            if (chrArray[start] != ' ') { //True if non-space (letter)
                 int end = start; //Use to know where the word is going to end
                 //Check if it is the latest letter and if next letter is not a space (entire word) increment
-                while(end + 1 < chrArray.length
+                while (end + 1 < chrArray.length
                         && chrArray[end + 1] != ' ') {
                     //Increase the window
                     end++; //move end to the end of the word
@@ -42,14 +43,12 @@ Output: "doG gniD"
         }
         return new String(chrArray);
     }
+
     private static void reverse(char[] ca, int start, int end) {
-        for(; start < end; end--) {
+        for (; start < end; end--) {
             char temp = ca[start];
             ca[start] = ca[end];
             ca[end] = temp;
         }
     }
-
-
-
 }
