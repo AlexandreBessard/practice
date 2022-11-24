1fa90f53525cf973ae31c42ffe1c09f7eaca9def
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-24 13:09:50 -0500

    MeetingsRoomsII

--- practice/src/leetcode/sortingAndSearching/MeetingsRoomsII.java
+++ practice/src/leetcode/sortingAndSearching/MeetingsRoomsII.java
@@ -7,8 +7,16 @@ import java.util.PriorityQueue;
 //https://leetcode.com/explore/interview/card/top-interview-questions-medium/110/sorting-and-searching/805/
 public class MeetingsRoomsII {
 /*
-Given an array of meeting time intervals intervals where intervals[i] = [starti, endi],
+Given an array of meeting time intervals where intervals[i] = [starti, endi],
 return the minimum number of conference rooms required.
+
+Example 1:
+Input: intervals = [[0,30],[5,10],[15,20]]
+Output: 2
+
+Example 2:
+Input: intervals = [[7,10],[2,4]]
+Output: 1
  */
     public static void main(String[] args) {
         int[][] nums = {
@@ -36,13 +44,13 @@ return the minimum number of conference rooms required.
         PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> a - b); //Smallest element are the priority
         Arrays.sort(intervals, (a, b) -> a[0] - b[0]); //sort by start
         minHeap.add(intervals[0][1]);
-        for(int i = 1; i < intervals.length; i++) {
+        for(int i = 1; i < intervals.length; i++) { //Loop through each interval
             int currentStart = intervals[i][0];
             int currentEnd = intervals[i][1];
             if(currentStart >= minHeap.peek()) { //true means not overlapping with the smallest element
                 minHeap.poll(); //Remove the meeting which end earlier
             }
-            minHeap.add(currentEnd);
+            minHeap.add(currentEnd); //We need a room
         }
         return minHeap.size();
     }
