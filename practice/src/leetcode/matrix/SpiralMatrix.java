59d33a0682bea4c6af076b7aaa811758eb87fdc1
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-24 15:43:35 -0500

    SpiralMatrix

--- practice/src/leetcode/matrix/SpiralMatrix.java
+++ practice/src/leetcode/matrix/SpiralMatrix.java
@@ -1,11 +1,13 @@
-package topInterviewQuestion.top100likedQuestions;
+package leetcode.matrix;
 
 import java.util.LinkedList;
 import java.util.List;
 
 //https://leetcode.com/problems/spiral-matrix/
 public class SpiralMatrix {
-
+/*
+Given an m x n matrix, return all elements of the matrix in spiral order.
+ */
     public static void main(String[] args) {
         int[][] matrix = {
                 {1, 2, 3},
@@ -33,17 +35,20 @@ public class SpiralMatrix {
         int up = 0, down = n - 1;
         int left = 0, right = m - 1;
         while (res.size() < n * m) { //Chekc if res is smaller than the total element in the matrix
+            //(1, 2, 3)
             for (int j = left; j <= right && res.size() < n * m; j++) //From left to right
                 res.add(matrix[up][j]);
 
-            //Excluded the latest element
-            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++) //From top to bottom right side
+            //Excluded the latest element  (6)
+            //From top (excluded) to bottom right side (excluded)
+            for (int i = up + 1; i <= down - 1 && res.size() < n * m; i++)
                 res.add(matrix[i][right]); //Right gets the latest element from the row 'i'
 
-            for (int j = right; j >= left && res.size() < n * m; j--) //From right to left
+            //From right to left (9, 8, 7)
+            for (int j = right; j >= left && res.size() < n * m; j--)
                 res.add(matrix[down][j]);
 
-            //Exclude the first element and the latest element
+            //Exclude the first element and the latest element (4)
             for (int i = down - 1; i >= up + 1 && res.size() < n * m; i--) { // (bottom to top)
                 System.out.println("matrix[i][left] --> " + matrix[i][left]);
                 res.add(matrix[i][left]);
