a17b904715c602938a8adf74f08e7ec510db49a9
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-26 16:43:52 -0500

    DesingInMemoryFileSystem

--- practice/src/leetcode/design/DesignInMemoryFileSystem.java
+++ practice/src/leetcode/design/DesignInMemoryFileSystem.java
@@ -1,6 +1,6 @@
 package leetcode.design;
 
-import java.util.List;
+import java.util.*;
 
 //https://leetcode.com/problems/design-in-memory-file-system/
 public class DesignInMemoryFileSystem {
@@ -21,28 +21,84 @@ If filePath already exists, appends the given content to original content.
 String readContentFromFile(String filePath) Returns the content in the file at filePath.
  */
     public static void main(String[] args) {
-
+        FileSystem fileSystem = new FileSystem();
+        System.out.println(fileSystem.ls("/"));                         // return []
+        fileSystem.mkdir("/a/b/c");
+        fileSystem.addContentToFile("/a/b/c/d", "hello");
+        System.out.println(fileSystem.ls("/"));                         // return ["a"]
+        System.out.println(fileSystem.ls("/a/b/c"));
+        System.out.println(fileSystem.readContentFromFile("/a/b/c/d")); // return "hello"
     }
 
+    //See image: practice/resources/fileSystem.png
     static class FileSystem {
+        Dir root;
         public FileSystem() {
-
+            root = new Dir();
         }
 
         public List<String> ls(String path) {
-            return null;
+            Dir currDir = root;
+            List<String> files = new ArrayList<>(); //Result
+            if(!path.equals("/")) { //true means it is a complete path
+                String[] dirs = path.split("/");
+                //Start to 1 because first element is empty -> ""
+                for(int i = 1; i < dirs.length - 1; i++) {
+                    currDir = currDir.dirs.get(dirs[i]);
+                }
+                if(currDir.files.containsKey(dirs[dirs.length - 1])) {
+                    files.add(dirs[dirs.length - 1]);
+                    return files;
+                } else {
+                    currDir = currDir.dirs.get(dirs[dirs.length - 1]);
+                }
+            }
+            files.addAll(new ArrayList<>(currDir.dirs.keySet()));
+            files.addAll(new ArrayList<>(currDir.files.keySet()));
+            Collections.sort(files);
+            return files;
         }
 
         public void mkdir(String path) {
-
+            Dir currDir = root;
+            String[] dir = path.split("/");
+            for(int i = 1; i < dir.length; i++) {
+                String d = dir[i];
+                if(!currDir.dirs.containsKey(d)) { //true if does NOT exist
+                    currDir.dirs.put(d, new Dir());
+                }
+                currDir = currDir.dirs.get(d);
+            }
         }
 
         public void addContentToFile(String filePath, String content) {
-
+            Dir currDir = root;
+            String[] dir = filePath.split("/");
+            for(int i = 1; i < dir.length - 1; i++) {
+                String d = dir[i];
+                currDir = currDir.dirs.get(d);
+            }
+            String latestDir = dir[dir.length - 1];
+            currDir.files.put(latestDir, currDir.files.getOrDefault(latestDir, "") + content);
         }
 
         public String readContentFromFile(String filePath) {
-            return null;
+            Dir currDir = root;
+            String[] dir = filePath.split("/");
+            for(int i = 1; i < dir.length - 1; i++) {
+                String d = dir[i];
+                currDir = currDir.dirs.get(d);
+            }
+            String latestDir = dir[dir.length - 1];
+            return currDir.files.get(latestDir);
         }
+    }
+
+    static class Dir {
+        //See image for better understanding : practice/resources/fileSystem.png
+        //Key: Subdirectory name, Value: subdirectory structure
+        Map<String, Dir> dirs = new HashMap<>();
+        //Key: file name, Value: Files content
+        Map<String, String> files = new HashMap<>();
     }
 }
