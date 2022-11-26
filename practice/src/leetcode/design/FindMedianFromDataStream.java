7b58730ba9be2e8299411059e54890e47a4e0354
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-26 14:22:15 -0500

    DesignInMemoryFileSystem

--- practice/src/leetcode/design/FindMedianFromDataStream.java
+++ practice/src/leetcode/design/FindMedianFromDataStream.java
@@ -1,11 +1,28 @@
-package topInterviewQuestion.amazon.design;
+package leetcode.design;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 
 //https://leetcode.com/explore/interview/card/amazon/81/design/495/
 public class FindMedianFromDataStream {
+/*
+The median is the middle value in an ordered integer list. If the size of the list is even, there is no middle value, and the median is the mean of the two middle values.
 
+For example, for arr = [2,3,4], the median is 3.
+For example, for arr = [2,3], the median is (2 + 3) / 2 = 2.5.
+Implement the MedianFinder class:
+
+MedianFinder() initializes the MedianFinder object.
+void addNum(int num) adds the integer num from the data stream to the data structure.
+double findMedian() returns the median of all elements so far. Answers within 10-5 of the actual answer will be accepted.
+
+Example 1:
+Input
+["MedianFinder", "addNum", "addNum", "findMedian", "addNum", "findMedian"]
+[[], [1], [2], [], [3], []]
+Output
+[null, null, null, 1.5, null, 2.0]
+ */
     public static void main(String[] args) {
         var obj = new MedianFinder();
         obj.addNum(1);
