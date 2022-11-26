999c9bca6084d838ced21df2e21a159aa1fdcdd6
AlexandreBessard <alexandre.bessard@gmail.com>
2022-11-25 19:34:12 -0500

    ShortestWordDistanceII & SearchSuggestionSystem

---
+++ practice/src/leetcode/design/SearchSuggestionSystem.java
@@ -0,0 +1,100 @@
+package leetcode.design;
+
+import java.util.*;
+
+//https://leetcode.com/problems/search-suggestions-system/
+public class SearchSuggestionSystem {
+    /*
+    You are given an array of strings products and a string searchWord.
+
+    Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+    Return a list of lists of the suggested products after each character of searchWord is typed.
+     */
+    public static void main(String[] args) {
+        String[] products = {"mobile", "mouse", "moneypot", "monitor", "mousepad"};
+        String[] products2 = {"him", "her", "his", "tim", "hi"};
+        String searchWord = "mouse";
+        for (List<String> list : suggestedProducts(products2, "h")) {
+            System.out.print(list);
+        }
+    }
+
+    //Approach 2: Trie + DFS
+    public static List<List<String>> suggestedProducts(String[] products,
+                                                       String searchWord) {
+
+        Trie trie = new Trie();
+        List<List<String>> result = new ArrayList<>();
+        //Add all words to the trie
+        for (String product : products) {
+            trie.insert(product);
+        }
+        //Initialize prefix
+        var prefix = new StringBuilder();
+        //Find word by prefix
+        for (char chr : searchWord.toCharArray()) {
+            prefix.append(chr);
+            result.add(trie.getWordsStartingWith(prefix.toString()));
+        }
+        return result;
+    }
+}
+
+class Trie {
+    Node root;
+    List<String> resultBuffer;
+
+    Trie() {
+        this.root = new Node();
+    }
+
+    public List<String> getWordsStartingWith(String prefix) {
+        Node currNode = this.root;
+        resultBuffer = new ArrayList<>();
+        for(char chr : prefix.toCharArray()) {
+            if(currNode.children.get(chr) == null) {
+                return resultBuffer;
+            }
+            currNode = currNode.children.get(chr);
+        }
+        dfsWithPrefix(currNode, prefix);
+        return resultBuffer;
+    }
+
+    //Iterative
+    private void dfsWithPrefix(Node currNode, String word) {
+        //Limit result size to 3
+        if(resultBuffer.size() == 3) { //Base case 1
+            return;
+        }
+        if(currNode.endOfWord) { //Add 1 word to the buffer
+            resultBuffer.add(word);
+        }
+        //Run DFS on all possible path
+        //lexicographically minimums products (from 'a' to 'z')
+        for(char chr = 'a'; chr <= 'z'; chr++) {
+            if(currNode.children.get(chr) != null) {
+                Node nextNode = currNode.children.get(chr);
+                dfsWithPrefix(nextNode, word + chr);
+            }
+        }
+    }
+
+    public void insert(String word) {
+        Node currNode = this.root;
+        for (char chr : word.toCharArray()) {
+            if(currNode.children.get(chr) == null) {
+                currNode.children.put(chr, new Node());
+            }
+            currNode = currNode.children.get(chr);
+        }
+        currNode.endOfWord = true;
+    }
+
+}
+
+class Node {
+    public Map<Character, Node> children = new HashMap<>();
+    public boolean endOfWord;
+}
---
+++ practice/src/leetcode/design/ShortestWordDistanceII.java
@@ -0,0 +1,75 @@
+package leetcode.design;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+//https://leetcode.com/problems/shortest-word-distance-ii/
+public class ShortestWordDistanceII {
+    /*
+    Design a data structure that will be initialized with a string array, and then it should answer queries of the shortest distance between two different strings from the array.
+
+    Implement the WordDistance class:
+
+    WordDistance(String[] wordsDict) initializes the object with the strings array wordsDict.
+    int shortest(String word1, String word2) returns the shortest distance between word1 and word2 in the array wordsDict.
+
+    Example 1:
+    Input
+    ["WordDistance", "shortest", "shortest"]
+    [[["practice", "makes", "perfect", "coding", "makes"]], ["coding", "practice"], ["makes", "coding"]]
+    Output
+    [null, 3, 1]
+     */
+    public static void main(String[] args) {
+        String[] words = {"practice", "makes", "perfect", "coding", "makes"};
+        WordDistance wordDistance = new WordDistance(words);
+        System.out.println(wordDistance.shortest("coding", "practice"));
+        System.out.println(wordDistance.shortest("makes", "coding"));
+    }
+
+    //Correction: https://leetcode.com/problems/shortest-word-distance-ii/discuss/67028/Java-Solution-using-HashMap
+    static class WordDistance {
+
+        //Key: word, Value: List of indexes where this word is located from the array
+        private Map<String, List<Integer>> map;
+
+        //Time: O(N)
+        //Space: O(N)
+        WordDistance(String[] words) {
+            map = new HashMap<>();
+            for (int currIndex = 0; currIndex < words.length; currIndex++) {
+                String currWord = words[currIndex];
+                if (map.containsKey(currWord)) {
+                    map.get(currWord).add(currIndex);
+                } else {
+                    map.put(currWord, new ArrayList<>(List.of(currIndex)));
+                }
+            }
+        }
+
+        public int shortest(String s1, String s2) {
+            List<Integer> list1 = map.get(s1);
+            List<Integer> list2 = map.get(s2);
+            int result = Integer.MAX_VALUE;
+            for (int i = 0, j = 0;
+                 i < list1.size() && j < list2.size(); ) {
+                int index1 = list1.get(i);
+                int index2 = list2.get(j);
+                if (index1 < index2) {
+                    int range = index2 - index1;
+                    result = Math.min(result, range);
+                    i++;
+                } else {
+                    int range = index1 - index2;
+                    result = Math.min(result, range);
+                    j++;
+                }
+            }
+            return result;
+        }
+    }
+
+
+}
